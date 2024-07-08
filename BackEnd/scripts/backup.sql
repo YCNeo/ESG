261a6a73c015004a0265d7e09261a3f79011235d
ching11720 <jasmine910402614@gmail.com>
2024-07-08 10:40:42 +0800

    build(DB): some edit, almost finished.

--- BackEnd/scripts/backup.sql
+++ BackEnd/scripts/backup.sql
@@ -1,24 +1,23 @@
-CREATE DATABASE IF NOT EXISTS `company`;
-USE `company`;
+USE ESG_db;
 
-DROP TABLE IF EXISTS `employees`;
-CREATE TABLE `employees` (
+CREATE TABLE IF NOT EXISTS `employees` (
   `EID` varchar(10) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `gender` int NOT NULL,
+  `gender` int NOT NULL check (gender=1 or gender=2),
   `email` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
+  `phone` varchar(11) NOT NULL,
   `nation` varchar(255) NOT NULL,
-  PRIMARY KEY (`EID`)
+  PRIMARY KEY (`EID`),
+  CONSTRAINT `check_email_format` CHECK (email REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
+  CONSTRAINT `check_phone_format` CHECK (phone REGEXP '^[0-9]+$')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `UID` varchar(10) NOT NULL,
   `password` varchar(255) NOT NULL,
   `access` varchar(255) NOT NULL,
-  PRIMARY KEY (`UID`)
-  FOREIGN KEY (`UID`) REFERENCES `employees` (`EID`)
+  PRIMARY KEY (`UID`),
+  CONSTRAINT FOREIGN KEY (`UID`) REFERENCES `employees` (`EID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 LOCK TABLES `employees` WRITE;
@@ -26,29 +25,25 @@ INSERT INTO `employees` VALUES (1,'Test1',1,'test1@mail.com','0911111111','TW');
 INSERT INTO `employees` VALUES (2,'Test2',1,'test2@mail.com','0922222222','USA');
 UNLOCK TABLES;
 
-DROP TABLE IF EXISTS `projects`;
-CREATE TABLE `projects` (
+CREATE TABLE IF NOT EXISTS `projects` (
   `PID` varchar(10) NOT NULL,
   `pname` varchar(255) NOT NULL,
   `flow` varchar(255) NOT NULL,
   `UID` varchar(10) NOT NULL,
-  PRIMARY KEY (`PID`)
-  FOREIGN KEY (`UID`) REFERENCES `users` (`UID`)
+  PRIMARY KEY (`PID`),
+  CONSTRAINT FOREIGN KEY (`UID`) REFERENCES `users` (`UID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `works_on`;
-CREATE TABLE `works_on` (
+CREATE TABLE IF NOT EXISTS `works_on` (
   `EID` varchar(10) NOT NULL,
   `PID` varchar(10) NOT NULL,
   `position` varchar(255) NOT NULL,
-  PRIMARY KEY (`EID`)
-  PRIMARY KEY (`PID`)
-  FOREIGN KEY (`EID`) REFERENCES `employees` (`EID`)
-  FOREIGN KEY (`PID`) REFERENCES `projects` (`PID`)
+  PRIMARY KEY (EID, PID),
+  CONSTRAINT FOREIGN KEY (`EID`) REFERENCES `employees` (`EID`),
+  CONSTRAINT FOREIGN KEY (`PID`) REFERENCES `projects` (`PID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `materials`;
-CREATE TABLE `materials` (
+CREATE TABLE IF NOT EXISTS `materials` (
   `MID` varchar(10) NOT NULL,
   `name` varchar(255) NOT NULL,
   `amount` int NOT NULL,
@@ -58,8 +53,7 @@ CREATE TABLE `materials` (
   PRIMARY KEY (`MID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `equipments`;
-CREATE TABLE `materials` (
+CREATE TABLE IF NOT EXISTS `equipments` (
   `EQID` varchar(10) NOT NULL,
   `name` varchar(255) NOT NULL,
   `amount` int NOT NULL,
@@ -71,52 +65,44 @@ CREATE TABLE `materials` (
   PRIMARY KEY (`EQID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `suppliers`;
-CREATE TABLE `materials` (
+CREATE TABLE IF NOT EXISTS `suppliers` (
   `SID` varchar(10) NOT NULL,
   `name` varchar(255) NOT NULL,
   `phone` varchar(11) NOT NULL,
   `nation` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
-  PRIMARY KEY (`SID`)
+  PRIMARY KEY (`SID`),
+  CONSTRAINT `check_sphone_format` CHECK (phone REGEXP '^[0-9]+$')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `usage_e`;
-CREATE TABLE `materials` (
+CREATE TABLE IF NOT EXISTS `usage_e` (
   `PID` varchar(10) NOT NULL,
   `EQID` varchar(10) NOT NULL,
-  PRIMARY KEY (`PID`)
-  PRIMARY KEY (`EQID`)
-  FOREIGN KEY (`PID`) REFERENCES `projects` (`PID`)
-  FOREIGN KEY (`EQID`) REFERENCES `equipments` (`EQID`)
+  PRIMARY KEY (EQID, PID),
+  CONSTRAINT FOREIGN KEY (`PID`) REFERENCES `projects` (`PID`),
+  CONSTRAINT FOREIGN KEY (`EQID`) REFERENCES `equipments` (`EQID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `usage_m`;
-CREATE TABLE `materials` (
+CREATE TABLE IF NOT EXISTS `usage_m` (
   `PID` varchar(10) NOT NULL,
   `MID` varchar(10) NOT NULL,
-  PRIMARY KEY (`PID`)
-  PRIMARY KEY (`MID`)
-  FOREIGN KEY (`PID`) REFERENCES `projects` (`PID`)
-  FOREIGN KEY (`MID`) REFERENCES `materials` (`MID`)
+  PRIMARY KEY (MID, PID),
+  CONSTRAINT FOREIGN KEY (`PID`) REFERENCES `projects` (`PID`),
+  CONSTRAINT FOREIGN KEY (`MID`) REFERENCES `materials` (`MID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `supply_e`;
-CREATE TABLE `materials` (
+CREATE TABLE IF NOT EXISTS `supply_e` (
   `SID` varchar(10) NOT NULL,
   `EQID` varchar(10) NOT NULL,
-  PRIMARY KEY (`SID`)
-  PRIMARY KEY (`EQID`)
-  FOREIGN KEY (`SID`) REFERENCES `suppliers` (`SID`)
-  FOREIGN KEY (`EQID`) REFERENCES `equipments` (`EQID`)
+  PRIMARY KEY (EQID, SID),
+  CONSTRAINT FOREIGN KEY (`SID`) REFERENCES `suppliers` (`SID`),
+  CONSTRAINT FOREIGN KEY (`EQID`) REFERENCES `equipments` (`EQID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `supply_m`;
-CREATE TABLE `materials` (
+CREATE TABLE IF NOT EXISTS `supply_m` (
   `SID` varchar(10) NOT NULL,
   `MID` varchar(10) NOT NULL,
-  PRIMARY KEY (`SID`)
-  PRIMARY KEY (`MID`)
-  FOREIGN KEY (`SID`) REFERENCES `suppliers` (`SID`)
-  FOREIGN KEY (`MID`) REFERENCES `materials` (`MID`)
+  PRIMARY KEY (MID, SID),
+  CONSTRAINT FOREIGN KEY (`SID`) REFERENCES `suppliers` (`SID`),
+  CONSTRAINT FOREIGN KEY (`MID`) REFERENCES `materials` (`MID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
